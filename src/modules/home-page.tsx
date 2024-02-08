92e4f6c0462d2e265a8975fa4d32fb0efdb063c2
Manas Khandelwal <mkdpsj@gmail.com>
2024-02-09 02:14:16 +0530

    add what is aalekh parallax

--- src/modules/home-page.tsx
+++ src/modules/home-page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { Parallax } from "react-parallax";
+
 import { ChancellorMessage } from "@/modules/chancellor-message";
 import { HeroSection } from "@/modules/hero-section";
 import { WhatIsAalekh } from "@/modules/what-is-aalekh";
@@ -8,6 +12,14 @@ export const HomePage: React.FC<HomePageProps> = ({}) => {
   return (
     <>
       <HeroSection />
+      <ChancellorMessage />
+      <WhatIsAalekh />
+      <Parallax
+        bgImage="/parallax-1.jpg"
+        bgImageAlt="sample photo"
+        strength={200}
+        className="h-[50vh]"
+      />
       <ChancellorMessage />
       <WhatIsAalekh />
     </>
