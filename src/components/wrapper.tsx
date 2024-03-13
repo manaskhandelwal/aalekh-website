8b0a82c03822765b2971fa9a7b6e80d9ce426094
Manas Khandelwal <mkdpsj@gmail.com>
2024-03-13 16:20:10 +0530

    add scroll margin

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -15,7 +15,7 @@ export const Wrapper: React.FC<WrapperProps> = ({
   return (
     <section
       className={cn(
-        `max-w-[1536px] mx-auto px-10 md:px-14 lg:px-16`,
+        `max-w-[1536px] mx-auto px-10 md:px-14 lg:px-16 scroll-mt-16`,
         className
       )}
       {...props}
