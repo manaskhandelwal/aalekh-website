913297f59da190b105a3e7def49c2740979e9675
Manas Khandelwal <mkdpsj@gmail.com>
2024-03-19 14:12:16 +0530

    fix: iframe accessibility

--- src/modules/footer.tsx
+++ src/modules/footer.tsx
@@ -34,6 +34,7 @@ export const Footer: React.FC<FooterProps> = ({}) => {
                 UP. India. 201310 <br />
               </p>
               <iframe
+                title="Google Map Link for Bennett University"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3507.9590369267953!2d77.58162287495004!3d28.450651192317874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cbf94deb6bc39%3A0x7ba6bedc9a2b537f!2sBennett%20University%20(Times%20of%20India%20Group)!5e0!3m2!1sen!2sin!4v1707510126183!5m2!1sen!2sin"
                 width={isMobile ? "400" : "230"}
                 height="150"
